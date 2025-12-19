6210c8218110f85aee0f2deb17e9bf26e5d88da7
vivek ranjan <vivekranjan8789@gmail.com>
2025-12-19 08:03:19 +0530

    about issue fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/About',
+        destination: '/about',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
